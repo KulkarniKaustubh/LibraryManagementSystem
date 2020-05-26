6cacc5a55586e1a84840ace82d5e7154b43ce310
KulkarniKaustubh <kmkapril15@gmail.com>
2020-05-27 00:11:23 +0530

    Updated join tests

--- sql_files/join_test.sql
+++ sql_files/join_test.sql
@@ -1,7 +1,13 @@
 SELECT cs.customerID, cs.branchID, customer.firstName, customer.lastName
 FROM customer_subscription AS cs
-JOIN customer ON customer.customerID = cs.customerID;
+INNER JOIN customer ON customer.customerID = cs.customerID;
 
 SELECT baddr.branchID, br.branchManager, baddr.region, baddr.city, baddr.state
 FROM branch_address AS baddr
-JOIN branch AS br ON br.branchID = baddr.branchID;
+INNER JOIN branch AS br ON br.branchID = baddr.branchID;
+
+SELECT * FROM books
+LEFT JOIN author ON books.authorID = author.authorID
+UNION
+SELECT * FROM books
+RIGHT JOIN author ON books.authorID = author.authorID;
